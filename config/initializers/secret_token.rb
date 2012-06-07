41f8c58ceedc2d17cbab54513255814bd0698583
Elior Avramoviz <elioravr@gmail.com>
2012-06-07 18:12:51 +0300

    Starting the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeverFlow::Application.config.secret_token = '81ae91d4b888741ec2b5dc6a6c3f670abedd1ee44ab2a70fb9b2cc812a6e361b57846effd2a8591640c9fdffe56009e1fa73d57132ce88f2f785cae80f3cefec'
